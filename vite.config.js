9700682542f3704931db7423daea7ae94006aacd
yukihiro-0127 <yukihiro0127szk@gmail.com>
2026-09-18 14:57:22 +0900

    fix: add base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,2 +1,2 @@
 import { defineConfig } from 'vite';
-export default defineConfig({build:{rollupOptions:{output:{manualChunks:{three:['three']}}}},server:{host:'127.0.0.1',port:5173,strictPort:true}});
+export default defineConfig({base:'/Astra6_Mariocart/',build:{rollupOptions:{output:{manualChunks:{three:['three']}}}},server:{host:'127.0.0.1',port:5173,strictPort:true}});
